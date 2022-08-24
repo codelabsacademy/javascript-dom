2145e7b3ea293fdbab1ebf5518c4e348695c9f07
thisisMoe <bounoua.smm@gmail.com>
2022-08-24 18:40:08 +0100

    Added countdown timer starter files

---
+++ Countdown Timer/script.js
@@ -0,0 +1,8 @@
+
+// We have controls to start a timer (class="timer__controls")
+// !!!!! to be able to have a countdown in our screen
+
+// Important: Create a countdown (Anytime 10s 20s)
+
+// timer(seconds)
+// timer(20) => a countdown of 20s
